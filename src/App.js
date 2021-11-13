fd46bf00bd8276d84d5f1f3aff422deb829f66e7
keyur diwan <keyurdiwan009@gmail.com>
2021-11-14 01:31:51 +0530

    create basics UI of income and expense box

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+import Details from './components/Details/Details';
+import useStyles from './styles';
+
+const App = () => {
+    const classes = useStyles();
+    return (
+        
+        <div >
+            <Grid className = {classes.grid} container spacing = {0} alignItems = "center" justify = "center" style = {{ height:'100vh '}}>
+                <Grid item xs = {12} sm = {4}> 
+                    <Details title = "Income"/>
+                </Grid>
+
+                <Grid item xs = {12} sm = {4}> 
+                   Main
+                </Grid>
+
+                <Grid item xs = {12} sm = {4}> 
+                    <Details title = "Expense" />
+                </Grid>
+
+            </Grid>
+            {/* App */}
+        </div>
+    )
+}
+export default App;
